4a24c096db4c93237a8b9ab52873c0f63ab0ebe1
JuanC2111 <76385515@continental.edu.pe>
2026-07-02 07:13:49 -0500

    Usar Tailwind CDN para produccion

--- resources/views/components/layouts/public.blade.php
+++ resources/views/components/layouts/public.blade.php
@@ -9,7 +9,40 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=poppins:400,500,600&family=fraunces:500,600&display=swap" rel="stylesheet" />
 
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <script src="https://cdn.tailwindcss.com"></script>
+<script>
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Poppins', 'sans-serif'],
+                display: ['Fraunces', 'serif'],
+            },
+            colors: {
+                bosque: {
+                    50: '#EAF3DE',
+                    100: '#C0DD97',
+                    200: '#97C459',
+                    400: '#639922',
+                    600: '#3B6D11',
+                    800: '#27500A',
+                    900: '#173404',
+                },
+                coral: {
+                    50: '#FAECE7',
+                    100: '#F5C4B3',
+                    200: '#F0997B',
+                    400: '#D85A30',
+                    600: '#993C1D',
+                    800: '#712B13',
+                    900: '#4A1B0C',
+                },
+                crema: '#FAF7F0',
+            },
+        },
+    },
+}
+</script>
     </head>
     <body class="font-sans antialiased bg-crema text-bosque-900">
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,40 @@
         <link href="https://fonts.bunny.net/css?family=poppins:400,500,600&family=fraunces:500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <script src="https://cdn.tailwindcss.com"></script>
+<script>
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Poppins', 'sans-serif'],
+                display: ['Fraunces', 'serif'],
+            },
+            colors: {
+                bosque: {
+                    50: '#EAF3DE',
+                    100: '#C0DD97',
+                    200: '#97C459',
+                    400: '#639922',
+                    600: '#3B6D11',
+                    800: '#27500A',
+                    900: '#173404',
+                },
+                coral: {
+                    50: '#FAECE7',
+                    100: '#F5C4B3',
+                    200: '#F0997B',
+                    400: '#D85A30',
+                    600: '#993C1D',
+                    800: '#712B13',
+                    900: '#4A1B0C',
+                },
+                crema: '#FAF7F0',
+            },
+        },
+    },
+}
+</script>
     </head>
     <body class="font-sans antialiased bg-crema">
         <div class="min-h-screen bg-gray-100">
